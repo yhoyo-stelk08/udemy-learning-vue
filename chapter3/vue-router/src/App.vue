939210c14617270458f9ff0b5d0ca78bb2d47147
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-16 23:20:17 +0800

    feat: add router to vue project
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.vue
    	modified:   src/main.js
    	new file:   src/router/index.js
    	new file:   src/views/AboutView.vue
    	new file:   src/views/HomeView.vue

--- chapter3/vue-router/src/App.vue
+++ chapter3/vue-router/src/App.vue
@@ -1,19 +1,30 @@
-<template></template>
-
-<script>
-  export default {
-    name: "App",
-    components: {},
-  };
-</script>
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
