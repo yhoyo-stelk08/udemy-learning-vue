4f32ca1307118811309a3dc96da4b80738987278
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-16 17:18:21 +0800

    feat: make new project called web-form
     Changes to be committed:
    	new file:   web-form/.browserslistrc
    	new file:   web-form/.gitignore
    	new file:   web-form/README.md
    	new file:   web-form/babel.config.js
    	new file:   web-form/jest.config.js
    	new file:   web-form/jsconfig.json
    	new file:   web-form/package-lock.json
    	new file:   web-form/package.json
    	new file:   web-form/public/favicon.ico
    	new file:   web-form/public/index.html
    	new file:   web-form/src/App.vue
    	new file:   web-form/src/assets/logo.png
    	new file:   web-form/src/main.js
    	new file:   web-form/tests/unit/example.spec.js
    	new file:   web-form/vue.config.js

---
+++ chapter2/web-form/src/App.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+<script>
+  export default {
+    name: "App",
+    components: {},
+  };
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #9aadc0;
+    margin-top: 60px;
+  }
+</style>
