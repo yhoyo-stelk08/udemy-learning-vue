30d7a1384c112c5474ce30e260a62f3ceb3f0723
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-06 20:21:33 +0800

    feat: learn about conditional rendering.

--- chapter1/app.js
+++ chapter1/app.js
@@ -3,11 +3,15 @@ const app = Vue.createApp({
     return {
       title: 'dynamic value',
       count: 0,
+      isShow: true,
     }
    },
    methods: {
     changeTitle(passedValue) {
       this.title = passedValue
+    },
+    showContent() {
+      this.isShow = !this.isShow
     }
    },
 })
