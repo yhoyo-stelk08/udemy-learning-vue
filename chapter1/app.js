235b1c5ee53aaae572f407113fecad155346e47d
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-06 20:07:42 +0800

    feat: learn about firing event in html page.

--- chapter1/app.js
+++ chapter1/app.js
@@ -1,41 +1,10 @@
 const app = Vue.createApp({
-  data() {
-    let context = {
-      visible: true,
-      persons: [
-        {
-          name: "Eko Prasetyo",
-          age: 40,
-          city: "Makassar",
-          isFav: false,
-        },
-        {
-          name: "Ucup",
-          age: 25,
-          city: "Ambon",
-          isFav: false,
-        },
-        {
-          name: "Reyhan",
-          age: 17,
-          city: "Ternate",
-          isFav: false,
-        },
-      ],
-    };
-    return context;
-  },
-  methods: {
-    toggleVisibility(e) {
-      // console.log(e.type);
-      this.visible = !this.visible;
-    },
-    toggleFav(fav, index) {
-      // console.log("initial isFav: " + fav, index);
-      this.persons[index].isFav = !fav;
-      // console.log("after change fav: " + this.persons[index].isFav);
-    },
-  },
-});
+   data() {
+    return {
+      title: 'dynamic value',
+      count: 0,
+    }
+   },
+})
 
-app.mount("#app");
+app.mount('#app')
