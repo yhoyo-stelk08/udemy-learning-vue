e6f68405e230c3a39a8d1ad3a5c70e3c6714246c
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-06 21:29:43 +0800

    feat: learn about using v-bind to render dynamic class.

--- chapter1/app.js
+++ chapter1/app.js
@@ -7,9 +7,9 @@ const app = Vue.createApp({
       x: 0,
       y: 0,
       books : [
-        {title: 'Title 1', author: 'Author 1'},
-        {title: 'Title 2', author: 'Author 2'},
-        {title: 'Title 3', author: 'Author 3'},
+        {title: 'Title 1', author: 'Author 1', img: 'assets/1.jpg', isFav: true},
+        {title: 'Title 2', author: 'Author 2', img: 'assets/2.jpg', isFav: false},
+        {title: 'Title 3', author: 'Author 3', img: 'assets/3.jpg', isFav: true},
       ],
       url: 'https://www.google.com',
     }
