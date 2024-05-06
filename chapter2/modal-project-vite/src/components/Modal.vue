5576c392f2a5530fb3936b40e45a37b57fffbb31
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-07 00:04:25 +0800

    feat: add styles to Modal components.

--- chapter2/modal-project-vite/src/components/Modal.vue
+++ chapter2/modal-project-vite/src/components/Modal.vue
@@ -1,11 +1,16 @@
 <template lang="">
-  <div class="backdrop-blur">
+  <div class="backdrop">
     <div class="modal">
         <p>modal content</p>
     </div>
   </div>
 </template>
-<script>
-export default {};
-</script>
-<style lang=""></style>
+
+<style>
+.modal {
+    @apply w-[400px] p-5 my-[100px] mx-auto bg-slate-100 rounded-xl
+}
+.backdrop {
+    @apply top-0 fixed bg-slate-900 w-full h-full
+}
+</style>
